85f7923419acb13fcc54f800b5525443bc7ca9c4
oWahyudi <carnegzs@gmail.com>
2023-11-04 17:50:47 +0800

    Add save newpage, editpage, random page,update CSS

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("create", views.create, name="create"),
     path("edit/<str:title>", views.edit, name="edit"),
+    path("randompage", views.randompage, name="randompage"),
 
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,11 @@
-import markdown2
-import html
+import markdown2, html,random
 
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponseRedirect
 from django.urls import reverse
 from . import util
 from .forms import NewPageForm,EditPageForm
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -43,25 +43,81 @@ def search(request):
             })
 
 def create(request):
-    if request.method=="post":
+    errormsg=""
+    iserror=False
+
+    if request.method=="POST":
         form=NewPageForm(request.POST)
+      
+        if form.is_valid():
+            title=form.cleaned_data['title_field']
+            content=form.cleaned_data['content_field']
+
+            if util.get_entry(title) is None:
+                try:
+                    util.save_entry(title,content)
+                    form=NewPageForm()
+                except Exception as e:
+                    errormsg="File save operation failed."
+                    iserror=True
+            else:
+                errormsg="The article is already exists."
+                iserror=True
+        else:
+            errormsg="The submitted data is invalid."
+            iserror=True
     else:
         form=NewPageForm()
+
         
     return render(request, "encyclopedia/newpage.html", {
         "form": form,
-        "errormessage": 'test',
-        'error': False
+        "errormessage": errormsg,
+        'iserror': iserror
     })
 
 def edit(request,title):
-    if request.method=="post":
-        form=EditPageForm(request.POST)
-    else:
-        form=EditPageForm(initial={'title_field': title})
+    errormsg=""
+    iserror=False
 
-    return render(request, "encyclopedia/newpage.html", {
+    if request.method=="POST":
+        form=EditPageForm(request.POST)
+    
+        if form.is_valid():
+            title=form.cleaned_data['title_field']
+            content=form.cleaned_data['content_field']
+
+            if util.get_entry(title) is not None:
+                try:
+                    util.save_entry(title,content)
+                    return HttpResponseRedirect(reverse("encyclopedia:create"))
+                except Exception as e:
+                    errormsg="File save operation failed."
+                    iserror=True
+            else:
+                errormsg="The article not exists in our repository."
+                iserror=True
+        else:
+            errormsg="The submitted data is invalid."
+            iserror=True
+
+    else:
+        content=util.get_entry(title)
+        if(content is None):
+            iserror=True
+            errormsg="The article not exists in our repository."
+
+        form=EditPageForm(initial={'title_field': title,'content_field':content})
+
+    return render(request, "encyclopedia/editpage.html", {
+        "title":'test',
         "form": form,
-        "errormessage": 'test',
-        'error': False
+        "errormessage": errormsg,
+        'iserror': iserror
     })
+
+def randompage(request):
+    entries= util.list_entries()
+    title=entries[random.randint(0, len(entries))]
+
+    return HttpResponseRedirect(reverse("encyclopedia:title",args=[title]))
